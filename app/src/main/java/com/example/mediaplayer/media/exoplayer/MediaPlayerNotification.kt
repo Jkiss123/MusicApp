8815413166a1764d3f3aa0f74dc7a79ccd74f1ff
jkiss123 <showparcloudy@gmail.com>
2024-05-13 00:32:11 +0700

    Commit 13/5

---
+++ app/src/main/java/com/example/mediaplayer/media/exoplayer/MediaPlayerNotification.kt
@@ -0,0 +1,65 @@
+package com.example.mediaplayer.media.exoplayer
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import com.example.mediaplayer.R
+import com.example.mediaplayer.media.constants.AudioContanst
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+
+internal class MediaPlayerNotification (
+    context:Context,
+    sessionToken:MediaSessionCompat.Token,
+    notificationListener : PlayerNotificationManager.NotificationListener
+){
+    private val notificationManager:PlayerNotificationManager
+
+    init {
+        val mediaController = MediaControllerCompat(context,sessionToken)
+        val builder= PlayerNotificationManager.Builder(
+            context,
+            AudioContanst.PLAYBACK_NOTIFICATION_ID,
+            AudioContanst.PLAYBACK_NOTIFICATION_CHANNEL_ID,
+
+        )
+        with(builder){
+            setMediaDescriptionAdapter(DescriptionAdapter(mediaController))
+            setNotificationListener(notificationListener)
+            setChannelNameResourceId(1)
+            setChannelDescriptionResourceId(2)
+
+        }
+        notificationManager = builder.build()
+        with(notificationManager){
+            setMediaSessionToken(sessionToken)
+            setSmallIcon(R.drawable.ic_music_circle)
+            setUseRewindAction(false)
+            setUseFastForwardAction(false)
+        }
+        fun hideNofitication(){
+            notificationManager.setPlayer(null)
+        }
+        fun showNotification(player:Player){
+            notificationManager.setPlayer(player)
+        }
+    }
+    inner class  DescriptionAdapter(private val controller:MediaControllerCompat) : PlayerNotificationManager.MediaDescriptionAdapter{
+        override fun getCurrentContentTitle(player: Player): CharSequence = controller.metadata.description.title.toString()
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent?= controller.sessionActivity
+
+        override fun getCurrentContentText(player: Player): CharSequence? =controller.metadata.description.subtitle
+
+        override fun getCurrentLargeIcon(
+            player: Player,
+            callback: PlayerNotificationManager.BitmapCallback
+        ): Bitmap? {
+            return null
+        }
+    }
+
+}
--- app/src/main/java/com/example/mediaplayer/media/service/MediaPlayerService.kt
+++ app/src/main/java/com/example/mediaplayer/media/service/MediaPlayerService.kt
@@ -1,12 +1,19 @@
 package com.example.mediaplayer.media.service
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
 import android.media.browse.MediaBrowser
 import android.os.Bundle
 import android.service.media.MediaBrowserService
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
+import androidx.core.content.ContextCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.example.mediaplayer.media.constants.AudioContanst
+import com.example.mediaplayer.media.exoplayer.MediaPlayerNotification
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.source.hls.DefaultHlsDataSourceFactory
 import com.google.android.exoplayer2.upstream.cache.CacheDataSource
@@ -14,9 +21,11 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import com.example.mediaplayer.media.exoplayer.MediaSource
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MediaPlayerService : MediaBrowserServiceCompat() {
@@ -35,6 +44,39 @@ class MediaPlayerService : MediaBrowserServiceCompat() {
     private lateinit var mediaSession:MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
 
+    private lateinit var mediaPlayerNotificationManager: MediaPlayerNotification
+    private var currentPlayingMedia:MediaMetadataCompat? = null
+    private val isPlayerInitialiez = false
+    var isForegroundService = false
+
+    companion object{
+        private const val  TAG = "MediaPlayerService"
+        var currentDuration :Long = 0L
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val sessionActivityIntent = packageManager?.getLaunchIntentForPackage(packageName)
+            ?.let { sessionIntent ->
+                PendingIntent.getActivity(this,0,sessionIntent,PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+            }
+
+        mediaSession = MediaSessionCompat(this, TAG).apply {
+            setSessionActivity(sessionActivityIntent)
+            isActive = true
+        }
+        sessionToken = mediaSession.sessionToken
+        mediaPlayerNotificationManager = MediaPlayerNotification(this,mediaSession.sessionToken,PlayerNotificationListener())
+        serviceScope.launch {
+            mediaSource.load()
+        }
+        mediaSessionConnector = MediaSessionConnector(mediaSession).apply {
+            setPlaybackPreparer()
+            setQueueNavigator()
+        }
+    }
+
     @Inject
     override fun onGetRoot(
         clientPackageName: String,
@@ -50,4 +92,26 @@ class MediaPlayerService : MediaBrowserServiceCompat() {
     ) {
         TODO("Not yet implemented")
     }
+
+    inner class PlayerNotificationListener : PlayerNotificationManager.NotificationListener{
+        override fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean) {
+            stopForeground(true)
+            isForegroundService = false
+            stopSelf()
+        }
+
+        @SuppressLint("ForegroundServiceType")
+        override fun onNotificationPosted(
+            notificationId: Int,
+            notification: Notification,
+            ongoing: Boolean
+        ) {
+            if (ongoing && !isForegroundService){
+                ContextCompat.startForegroundService(applicationContext, Intent(applicationContext,this@MediaPlayerService.javaClass))
+                startForeground(notificationId,notification)
+                isForegroundService = true
+            }
+        }
+    }
+    inner class AudioMediaPlayBackPrepare:
 }
